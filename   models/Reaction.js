7cfb8c69500fd72487cf07c5f037d57abb75ff84
carolpang <carolpang17@gmail.com>
2022-04-26 18:07:51 -0700

    added server and models

---
+++   models/Reaction.js
@@ -0,0 +1,28 @@
+const { Schema, Types } = require('mongoose');
+const  dateFormat  = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            validate: [({ length }) => length <= 280, 'Reaction must be more than 1 characters and less than 280 characters!']
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    }
+)
+
+
+module.exports = ReactionSchema;
